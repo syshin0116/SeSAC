1356a82ad3a764a5a003be2a0d5566790a58bcc6
syshin0116 <syshin0116@gmail.com>
2023-08-22 21:51:45 +0900

    news

---
+++ PCCP/practice.py
@@ -0,0 +1,13 @@
+# # 숫자 입력 받기
+# num = input()
+# int_num = int(num)
+# print(type(num))
+# print(type(int_num))
+
+
+# # 숫자 2개 입력 받기
+# num1 = int(input('num1:'))
+# num2 = int(input("num2:"))
+# print(num1 + num2)
+
+# 수요일까지 변수, 리스트, 딕셔너리, 메서드 한번씩 써보기
